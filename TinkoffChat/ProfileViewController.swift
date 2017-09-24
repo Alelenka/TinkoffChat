f795733d936d519b0378b2ea5c1b074089a16772
Alyona Belyaeva <alenko31@gmail.com>
2017-09-24 11:56:22 +0300

    VC lifecicle log

--- TinkoffChat/ProfileViewController.swift
+++ TinkoffChat/ProfileViewController.swift
@@ -10,16 +10,48 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    //MARK: - Lifecicle
     override func viewDidLoad() {
-        super.viewDidLoad()
-
+        super.viewDidLoad() // Always
+        logInfo()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated) // Always
+        logInfo()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated) // Always
+        logInfo()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews() //Not neccessary
+        logInfo()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews() //Not neccessary
+        logInfo()
+    }
+    
+    override func  viewWillDisappear(_ animated: Bool) {
+        super .viewWillDisappear(animated) // Always
+        logInfo()
+    }
+    override func  viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated) // Always
+        logInfo()
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning() // Always
+//      automated log
     }
 
-
+    private func logInfo (string: String = #function) {
+        print("ViewConstoller method : \(string)")
+    }
 }
 
