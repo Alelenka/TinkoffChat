77d5232398ed50af092d38d0b1dac0941e6dd0ef
Alyona Belyaeva <alenko31@gmail.com>
2017-09-24 10:23:19 +0300

    Add files

---
+++ TinkoffChat/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinkoffChat
+//
+//  Created by Alyona Belyaeva on 20.09.17.
+//  Copyright © 2017 AB. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
